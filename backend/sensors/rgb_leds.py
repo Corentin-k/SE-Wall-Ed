3c42a999824835adf1e9861c1bb4693be6499742
Arnaud <arnaud.grivel@efrei.net>
2025-06-10 14:10:17 +0200

    add test led

--- backend/sensors/rgb_leds.py
+++ backend/sensors/rgb_leds.py
@@ -1,4 +1,36 @@
-﻿class RGBLEDs:
-    def __init__(self):
+﻿import RPi.GPIO as GPIO
+import time
+
+
+def switchSetpup() :
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(9, GPIO.OUT)  
+    GPIO.setup(25, GPIO.OUT)  
+    GPIO.setup(11, GPIO.OUT)  
+
+def switch(status, gpio):
+    if status == 1:
+        GPIO.output(gpio, GPIO.HIGH)
+    elif status == 0:
+        GPIO.output(gpio, GPIO.LOW)
+    else:
         pass
-        
+
+
+
+def main():
+    switchSetpup()
+    run = True
+    while run :
+        switch(1, 9)
+        switch(1, 25)
+        switch(1, 11)
+        time.sleep(1)
+        switch(0, 9)
+        switch(0, 25)
+        switch(0, 11)
+        time.sleep(1)
+
+main()
